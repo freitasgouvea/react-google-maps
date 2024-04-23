f71595e61d5d8e4f6bb1c157ea42acb964c4dbd1
Flavio Gouvea <46804396+freitasgouvea@users.noreply.github.com>
2024-04-22 22:30:22 -0300

    Update spot.jsx

--- frontend/src/components/spot/spot.jsx
+++ frontend/src/components/spot/spot.jsx
@@ -13,7 +13,7 @@ const headerProps = {
 const baseUrl = 'http://localhost:3001/spots'
 
 const loader = new Loader({
-    apiKey: "AIzaSyBTdaly7le-SH4H3nEofhl2UEWWYpX0WJE",
+    apiKey: process.env.API_KEY,
     version: "weekly",
     libraries: ["places"]
 });
